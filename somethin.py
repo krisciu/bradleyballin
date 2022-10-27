6bc5656bc198a77171d67d588b48c63ef1c2b972
krisciu <krisciu@gmail.com>
2022-10-26 22:09:10 -0500

    trying options

--- somethin.py
+++ somethin.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome import service
 from webdriver_manager.chrome import ChromeDriverManager
 
+options = ChromeOptions()
+options.add_argument("--no-sandbox")
 service = Service(executable_path=ChromeDriverManager().install())
 
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=service,options=options)
 driver.get("https://www.cincinnati.com/story/sports/high-school/high-school-sports/2022/10/25/vote-cincinnati-enquirer-high-school-athletes-of-the-week-oct-24/69585959007/")
 survey = driver.find_element(By.ID, "PDI_answer51312243")
 button = driver.find_element(By.ID, "pd-vote-button11227228")
