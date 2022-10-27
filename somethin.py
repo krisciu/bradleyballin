6fb599b66818a9d307e3112a73329823d0b3628b
krisciu <krisciu@gmail.com>
2022-10-26 23:39:43 -0500

    tst

--- somethin.py
+++ somethin.py
@@ -15,7 +15,7 @@ driver = webdriver.Chrome(service=service,options=options)
 driver.get("https://www.cincinnati.com/story/sports/high-school/high-school-sports/2022/10/25/vote-cincinnati-enquirer-high-school-athletes-of-the-week-oct-24/69585959007/")
 #/html/body/div[2]/main/article/div[4]/aside[13]/iframe
 test = driver.find_element(By.XPATH, "/html/body/div[2]/main/article/div[4]/aside[13]")
-frame = test.find_element(By.XPATH, "/html/body/div[2]/main/article/div[4]/aside[13]/iframe")
+frame = test.find_element(By.CLASS_NAME, "gnt_em_hf_if")
 driver.switch_to.frame(frame)
 survey = driver.find_element(By.XPATH, "/html/body/div/form/div/div/div/div/div[2]/span")
 surveyclicker = survey.find_element(By.ID, "PDI_answer51312243")
