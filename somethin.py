92a0f39a24e39b96faa5c6f8187dbd2d37718900
krisciu <krisciu@gmail.com>
2022-10-27 00:37:07 -0500

    tst

--- somethin.py
+++ somethin.py
@@ -13,14 +13,18 @@ options.add_argument("--headless")
 service = Service(executable_path=ChromeDriverManager().install())
 
 driver = webdriver.Chrome(service=service,options=options)
+
 driver.get("https://www.cincinnati.com/story/sports/high-school/high-school-sports/2022/10/25/vote-cincinnati-enquirer-high-school-athletes-of-the-week-oct-24/69585959007/")
 #/html/body/div[2]/main/article/div[4]/aside[13]/iframe
 test = driver.find_element(By.XPATH, "/html/body/div[2]/main/article/div[4]/aside[13]")
 #frame = test.find_element(By.CLASS_NAME, "gnt_em_hf_if")
-driver.switch_to.frame(13)
+driver.implicitly_wait(5)
+
+driver.switch_to.frame(driver.find_element(By.XPATH, "/html/body/div[2]/main/article/div[4]/aside[13]/iframe"))
 #survey = driver.find_element(By.XPATH, "/html/body/div/form/div/div/div/div/div[2]/span")
-test = driver.find_element(By.XPATH, "/html/body/div/form")
 surveyclicker = driver.find_element(By.ID, "PDI_answer51312243")
+test = driver.find_element(By.XPATH, "/html/body/div/form")
+
 button = driver.find_element(By.ID, "pd-vote-button11227228")
 
 surveyclicker.click()
