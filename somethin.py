1f1ac26a2c00bd1e5e6281d6c6fc80df6772f6f1
krisciu <krisciu@gmail.com>
2022-10-26 21:50:56 -0500

    lol

--- somethin.py
+++ somethin.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from chromedriver_py import binary_path
 
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+service_object = Service(binary_path)
+driver = webdriver.Chrome(service=service_object)
 driver.get("https://www.cincinnati.com/story/sports/high-school/high-school-sports/2022/10/25/vote-cincinnati-enquirer-high-school-athletes-of-the-week-oct-24/69585959007/")
 survey = driver.find_element(By.ID, "PDI_answer51312243")
 button = driver.find_element(By.ID, "pd-vote-button11227228")
